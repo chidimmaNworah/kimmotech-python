5df0c846f60965ee2d6a8fe7ef85284e356126ea
chidimmaNworah <chidimmanworah12@gmail.com>
2025-03-18 16:49:58 +0100

    cors origin challenge

--- app/main.py
+++ app/main.py
@@ -24,7 +24,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://www.kimmotech.net"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
