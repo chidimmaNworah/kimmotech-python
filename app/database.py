fb2e71203f5878a77f78cb67b6ca8f2dcd230b0b
chidimmaNworah <chidimmanworah12@gmail.com>
2025-04-15 14:50:05 +0100

    building db from scratch2

--- app/database.py
+++ app/database.py
@@ -5,16 +5,16 @@ from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 load_dotenv()
 # Database Configuration from env
-# DB_NAME = os.getenv("DB_NAME")
-# DB_USER = os.getenv("DB_USER")
-# DB_PASSWORD = os.getenv("DB_PASSWORD")
-# DB_HOST = os.getenv("DB_HOST")
-# DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
 
-# DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-DATABASE_URL = os.getenv("DB_URL")
-print("DATABASE_URL:", DATABASE_URL)
+# DATABASE_URL = os.getenv("DB_URL")
+# print("DATABASE_URL:", DATABASE_URL)
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL is not set in environment variables!")
 
